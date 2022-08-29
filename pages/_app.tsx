99990313741a8430770517696e34d4c0a375eb70
Salief Lewis <salieflewis@gmail.com>
2022-08-29 10:53:54 -0400

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@fontsource/inter/400.css';
+// import '@fontsource/inter/600.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@zoralabs/zord/index.css';
+import { ThemeProvider, lightTheme } from '@zoralabs/zord';
+import type { AppProps } from 'next/app';
+
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps) {
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-
-export default MyApp
