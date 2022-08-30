7ed29727d7adc645c010f8720b3f422a357f8fd5
Salief Lewis <salieflewis@gmail.com>
2022-08-30 09:37:45 -0400

    removed google fonts style tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,10 +20,6 @@ const Home: NextPage = () => {
           sizes='32x32'
           href='/favicon-32x32.png'
         />
-        <style>
-          {/* @import */}
-          url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap');
-        </style>
       </Head>
       <Header />
       <Feed />
