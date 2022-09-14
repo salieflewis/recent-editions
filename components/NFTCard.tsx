784fecd1526153efb67d8751d896a80c54665c3d
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2022-09-14 19:39:18 -0400

    fix: handle bug with empty string media

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -1,12 +1,9 @@
 import {
   Box,
-  Button,
   Flex,
-  Eyebrow,
   Label,
   Tag,
 } from '@zoralabs/zord';
-import { IpfsImage } from 'react-ipfs-image';
 
 import {
   cardWrapper,
@@ -18,7 +15,6 @@ import { processImgURI } from 'utils/formatters';
 
 import { Collect } from './Collect';
 import Image from 'next/image'
-import layout from './Layout';
 
 export const NFTCard = ({
   editionMetadata,
@@ -27,22 +23,20 @@ export const NFTCard = ({
   address,
   publicSalePrice,
 }) => {
+
   return (
     <Box className={cardWrapper}>
       <Box>
-        {/* <IpfsImage
-          className={imageWrapper}
-          hash={editionMetadata.imageURI}
-          gatewayUrl='https://zora-dev.mypinata.cloud/ipfs'
-        /> */}
         <Box className={imageWrapper}>
           {/* Note: vercels image optimization does dull out some colors but the file savings are tremendous in comparison */}
-          <Image src={processImgURI(editionMetadata.imageURI)} alt={name} layout='fill' className={image} />
+          {/* Handle case where zora allows user to mint broken media which returns as empty strings "". Could use placeholders here. */}
+          {editionMetadata.imageURI !== "" &&
+            <Image src={processImgURI(editionMetadata.imageURI)} alt={name} layout='fill' className={image} />
+          }
         </Box>
-        {/* <Box mx='x3' mb='x3' maxW='x64'> */}
-        <Box mx='x3' my='x2' maxW='x64'>
+        <Box mx='x3' my='x2' maxW='100%'>
           <Flex justify='space-between' align='center'>
-            <Label size='sm' className={cardTitle}>
+            <Label size='lg' className={cardTitle}>
               {name}
             </Label>
             <Tag active={true}>${symbol}</Tag>
