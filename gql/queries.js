56708ddad0c808fe1ff1da5744fbe91a896649f7
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2022-09-14 11:38:58 -0400

    add: free drop query

--- gql/queries.js
+++ gql/queries.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
-export const newDrops = gql`
-  query ($first: Int, $skip: Int) {
+export const GET_NEW_DROPS = gql`
+  query ERC721Drops($first: Int, $skip: Int) {
     erc721Drops(
       orderBy: createdAt
       orderDirection: desc
@@ -21,3 +21,29 @@ export const newDrops = gql`
     }
   }
 `;
+
+export const GET_FREE_DROPS = gql`
+query FreeERC721Drops($first: Int, $skip: Int, $orderDirection: String) {
+  erc721Drops(
+    orderBy: createdAt
+    orderDirection: desc
+    first: $first
+    skip: $skip
+    where: {salesConfig_: {publicSalePrice: "0"}}
+  ) {
+    name
+    address
+    createdAt
+    owner
+    symbol
+    salesConfig {
+      publicSalePrice
+    }
+    id
+    editionMetadata {
+      imageURI
+      id
+    }
+  }
+}
+`
